4cdd4482706882df00886e5b58ac1da0510cbd69
fujisan <gashigashi3040@gmail.com>
2020-05-13 02:02:39 +0900

    #22. v-onディレクティブの引数を動的に表現する

--- js/main.js
+++ js/main.js
@@ -3,20 +3,12 @@ new Vue({
   //data: プロパティを書ける
   data: {
     number: 0,
-    x: 0,
-    y: 0,
+    event: 'click',
   },
   //methods: 関数宣言の場所
   methods: {
     countUp: function(times) {
       this.number += 1 * times;
     },
-    changeMousePosition: function(divideNumber, event) {
-      this.x = event.clientX / divideNumber;
-      this.y = event.clientY / divideNumber;
-    },
-    myAlert() {
-      alert('指をキーボードから離しました');
-    }
   }
 })
