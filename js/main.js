1baa823d1434006fd8fd3e36e16bc00d47a17018
fujisan <gashigashi3040@gmail.com>
2020-05-13 02:27:41 +0900

    4# 双方向データバインディングを実現するためのv-model

--- js/main.js
+++ js/main.js
@@ -2,13 +2,10 @@ new Vue({
   el: '#app',
   //data: プロパティを書ける
   data: {
-    number: 0,
-    event: 'click',
+    message: 'good morning',
   },
   //methods: 関数宣言の場所
   methods: {
-    countUp: function(times) {
-      this.number += 1 * times;
-    },
+
   }
 })
