4a28a957f2927e1d47c5ba9d0f4a65c480fd7930
fujisan <gashigashi3040@gmail.com>
2020-05-13 18:05:55 +0900

    #29. クラスをデータにバインディング(紐付け)する方法その１。オブジェクトを渡し、真偽値を使って動的に適応させるクラスを切り替える

--- js/main.js
+++ js/main.js
@@ -3,22 +3,20 @@ new Vue({
   //data: プロパティを書ける
   // データでは、動的なものを扱うことはできない。あくまで、初期値を扱うもの。
   data: {
-    counter: 0,
+    isActive: true
   },
   //computed : メソッドを処理をプロパティにしたもの
   computed: {
-    doubleCountComouted: function() {
-      return this.counter * 2;
+    classObject: function() {
+      return {
+      red: this.isActive,
+      'bg-blue': !this.isActive
+      }
     }
   },
   //methods: 関数宣言の場所
   methods: {
-    countUp: function() {
-      return this.counter += 1;
-    },
-    doubleCountMethod: function() {
-      return this.counter * 2;
-    }
+
   },
   //watch
   //ウォッチャと呼ばれ、データが変更されたタイミングで処理を実行したいときに使います。（非同期の処理など） watchオプションのなかで「変更を監視したい変数: function() 」で関数を定義します。 定義する変数はdataプロパティと紐付いている必要があります。
