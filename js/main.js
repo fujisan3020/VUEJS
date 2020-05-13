77a982a322c82e2b9f5b0b715e6d4198fec1e928
fujisan <gashigashi3040@gmail.com>
2020-05-13 17:47:31 +0900

    #28 丸カッコ( )は、二重中括弧とv-onディレクティブにおいて、いつ必要なのか

--- js/main.js
+++ js/main.js
@@ -4,33 +4,25 @@ new Vue({
   // データでは、動的なものを扱うことはできない。あくまで、初期値を扱うもの。
   data: {
     counter: 0,
-    otherCounter: 0
-  },
-  //methods: 関数宣言の場所
-  methods: {
-
   },
   //computed : メソッドを処理をプロパティにしたもの
   computed: {
-    lessThanThree: function() {
-      console.log('Computed');
-      return this.counter > 3 ? '3以上' : '3以下'
+    doubleCountComouted: function() {
+      return this.counter * 2;
     }
   },
-  // computedとmethodsの違い
-  // 1. 両者はプロパティとメソッドであること
-  // 2. computedはキャッシュされる
-  // ※2の補足 computedは元データの変更があった時点でデータが保存されるのに対して、method側はいつでもプログラムを実行して値を計算するというこ
-
-  //watch
-  //ウォッチャと呼ばれ、データが変更されたタイミングで処理を実行したいときに使います。（非同期の処理など）
-  //watchオプションのなかで「変更を監視したい変数: function() 」で関数を定義します。 定義する変数はdataプロパティと紐付いている必要があります。
-  watch: {
-    counter: function() {
-      var vm = this;
-      setTimeout(function() {
-        vm.counter = 0
-      }, 3000)
+  //methods: 関数宣言の場所
+  methods: {
+    countUp: function() {
+      return this.counter += 1;
+    },
+    doubleCountMethod: function() {
+      return this.counter * 2;
     }
+  },
+  //watch
+  //ウォッチャと呼ばれ、データが変更されたタイミングで処理を実行したいときに使います。（非同期の処理など） watchオプションのなかで「変更を監視したい変数: function() 」で関数を定義します。 定義する変数はdataプロパティと紐付いている必要があります。
+  watch: {
+
   }
 })
