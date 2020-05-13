c4f12cae778ee1c8de5d99fc0fa5c842575e7af7
fujisan <gashigashi3040@gmail.com>
2020-05-13 17:25:08 +0900

    #27 データが変わった時に特定の処理をするwatch

--- js/main.js
+++ js/main.js
@@ -8,21 +8,29 @@ new Vue({
   },
   //methods: 関数宣言の場所
   methods: {
-    lessThanThreeMethod: function() {
-      console.log('Method');
-      return this.counter > 3 ? '3以上' : '3以下'
-    }
+
   },
   //computed : メソッドを処理をプロパティにしたもの
   computed: {
     lessThanThree: function() {
       console.log('Computed');
       return this.counter > 3 ? '3以上' : '3以下'
-    },
-  }
+    }
+  },
   // computedとmethodsの違い
   // 1. 両者はプロパティとメソッドであること
   // 2. computedはキャッシュされる
   // ※2の補足 computedは元データの変更があった時点でデータが保存されるのに対して、method側はいつでもプログラムを実行して値を計算するというこ
-  //
+
+  //watch
+  //ウォッチャと呼ばれ、データが変更されたタイミングで処理を実行したいときに使います。（非同期の処理など）
+  //watchオプションのなかで「変更を監視したい変数: function() 」で関数を定義します。 定義する変数はdataプロパティと紐付いている必要があります。
+  watch: {
+    counter: function() {
+      var vm = this;
+      setTimeout(function() {
+        vm.counter = 0
+      }, 3000)
+    }
+  }
 })
