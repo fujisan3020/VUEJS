6886e936daa817487127e447db72584b85c1e499
fujisan <gashigashi3040@gmail.com>
2020-05-22 08:55:55 +0900

    #76 コンポーネントにおけるグローバル登録とローカル登録

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
-// componentのdataは関数であり、returnで返す必要がある。
-Vue.component('my-component', {
+// コンポーネントにおけるローカル登録
+// (グローバル登録と比べて、ローカル登録の方がよく使用される) 
+var component = {
   data: function() {
     return {
-      number: 12,
+      number: 12
     }
   },
-  template: '<p>いいね({{ number }})</p>',
-})
+  template: '<p>いいね({{ number }})<button @click="increment">+1</button></p>',
+  methods: {
+    increment: function() {
+      this.number += 1;
+    }
+  }
+}
 
 new Vue({
   el: '#app',
+  components: {
+    'my-component': component
+  }
 })
+
+new Vue({
+  el: '#app2',
+})
+
+
+// componentのdataは基本的に関数であり、returnで返す必要がある。
+// Vue.component('my-component', {
+//   data: function() {
+//     return {
+//       number: 12,
+//     }
+//   },
+//   template: '<p>いいね({{ number }})</p>',
+// })
