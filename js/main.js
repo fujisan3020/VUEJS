8e9d2605f82771e97037fb3f38e75cb1c7d8cafd
fujisan <gashigashi3040@gmail.com>
2020-05-22 08:37:10 +0900

    #74~75 componentとdata(と関数)

--- js/main.js
+++ js/main.js
@@ -1,103 +1,13 @@
-var data = {
-  message: 'こんにちは',
-  name: 'よしぴー',
-}
-
-// コンポーネント : インスタンスを複数使い回すパーツ
-Vue.component('hello', {
-  template: '<p>hello</p>'
-});
-
-
-var vm = new Vue({
-  // el: '#app',
-  //data: プロパティを書ける
-  data: data,
-  //computed : メソッドを処理をプロパティにしたもの
-  computed: {
-    myData: function() {
-      return this.$data
+// componentのdataは関数であり、returnで返す必要がある。
+Vue.component('my-component', {
+  data: function() {
+    return {
+      number: 12,
     }
   },
-  //methods: 関数宣言の場所
-  methods: {
-
-  },
-  //watch
-  //ウォッチャと呼ばれ、データが変更されたタイミングで処理を実行したいときに使います。（非同期の処理など） watchオプションのなかで「変更を監視したい変数: function() 」で関数を定義します。 定義する変数はdataプロパティと紐付いている必要があります。
-  watch: {
-
-  }
+  template: '<p>いいね({{ number }})</p>',
 })
-// $mountメソッド : elプロパティを後付けするメソッド
-// 基本的には使用しないが、たまに使う。elプロパティを動的に変化させる時やlプロパティを後から設定する時などに使う
-vm.$mount('#app')
-
 
 new Vue({
-  // el: '#app2',
-  data: {
-    name: 'よしぴー'
-  },
-  // template(プロパティ) : template構文の他の記載方法(1)
-  template: '<h1>こんにちは、{{ name }}</h1>'
-}).$mount('#app2')
-// $mountはVueインスタンスの最後に付け加えるような記載もできる
-
-
-new Vue({
-  data: {
-    name: 'よしぴー'
-  },
-  // render(関数) : template構文の他の記載方法(2)
-  // 仮想ノードを作ってDOMの描画を行う
-  render: function(createElement) {
-    console.log(createElement('h1', 'こんにちは、' + this.name));
-    // createElement引数(第一引数：タグ, 第二引数：値)
-    return createElement('h1', 'こんにちは、' + this.name);
-  }
-}).$mount('#app3')
-
-// このcreateElementは直接DOMにアクセスしている
-var dir = document.createElement('div');
-console.log(dir);
-console.dir(dir);
-console.log(document);
-console.dir(document);
-
-
-new Vue({
-  el:'#app4',
-  data: {
-    name: 'よしぴー'
-  },
-  beforeCreate: function() {
-    console.log('beforeCreate');
-  },
-  created: function() {
-    console.log('created');
-  },
-  beforeMount: function() {
-    console.log('beforeMount');
-  },
-  mounted: function() {
-    console.log('mounted');
-  },
-  beforeUpdate: function() {
-    console.log('beforeUpdate');
-  },
-  updated: function() {
-    console.log('updated');
-  },
-  beforeDestory: function() {
-    console.log('beforeDestory');
-  },
-  destoryed: function() {
-    console.log('destoryed');
-  },
-  methods: {
-    destory: function() {
-      this.$destroy()
-    }
-  }
+  el: '#app',
 })
